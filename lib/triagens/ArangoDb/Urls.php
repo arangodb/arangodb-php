b146ebd57883f40d9ee5cc66075001f37a2e2f9a
Frank Mayer <frank@frankmayer.net>
2013-02-16 09:04:27 +0200

    Defined some url parts for the graph API implementation
    Signed-off-by: Frank Mayer <frank@frankmayer.net>

--- lib/triagens/ArangoDb/Urls.php
+++ lib/triagens/ArangoDb/Urls.php
@@ -32,6 +32,16 @@ abstract class URLs {
   const URL_GRAPH               = '/_api/graph';
 
   /**
+   * URL base part for all document-related REST calls
+   */
+  const URLPART_VERTEX               = 'vertex';
+
+  /**
+   * URL base part for all document-related REST calls
+   */
+  const URLPART_EDGE               = 'edge';
+
+  /**
    * URL base part for all collection-related REST calls
    */
   const URL_COLLECTION         = '/_api/collection';
