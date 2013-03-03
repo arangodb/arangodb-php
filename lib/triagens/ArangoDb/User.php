48a8345fa41fc6409d54f5080f00bebe7d98df94
Frank Mayer <frank@frankmayer.net>
2013-03-03 02:03:31 +0200

    Implemented User Management (+ Test & Docs)
    Signed-off-by: Frank Mayer <frank@frankmayer.net>

---
+++ lib/triagens/ArangoDb/User.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * ArangoDB PHP client: single user document
+ *
+ * @package   ArangoDbPhpClient
+ * @author    Frank Mayer
+ * @since     1.2
+ */
+
+namespace triagens\ArangoDb;
+
+/**
+ * Value object representing a single User document
+ *
+ * @package   ArangoDbPhpClient
+ * @since     1.2
+ */
+class User extends
+    Document
+{
+
+}
