521ac843e558df5426a04be48adb7a81fc5d699e
F21 <francis.chuang@gmail.com>
2013-03-26 21:19:26 +1100

    Moved spl_autoload_register to init() of the autoloader.

--- autoload.php
+++ autoload.php
@@ -5,5 +5,3 @@ namespace triagens\ArangoDb;
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'triagens' . DIRECTORY_SEPARATOR . 'ArangoDb' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 
 Autoloader::init();
-
-spl_autoload_register(__NAMESPACE__ . '\Autoloader::load');
