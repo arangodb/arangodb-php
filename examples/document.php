d5fc60857accc76ff396247c6ea218ea7995fb3d
Frank Mayer <frank@frankmayer.net>
2013-05-07 01:29:08 +0300

    Cleaning spree => examples

--- examples/document.php
+++ examples/document.php
@@ -6,55 +6,54 @@ require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'init.php';
 
 
 try {
-  $connection = new Connection($connectionOptions);
-  $handler = new DocumentHandler($connection);
+    $connection = new Connection($connectionOptions);
+    $handler    = new DocumentHandler($connection);
 
-  // create a new document
-  $user = new Document();
-  $user->set("name", "John");
-  $user->age=19;
+    // create a new document
+    $user = new Document();
+    $user->set("name", "John");
+    $user->age = 19;
 
-  $id = $handler->add("users", $user);
+    $id = $handler->add("users", $user);
 
-  // get documents by example
-  $cursor = $handler->getByExample("users", array("name"=>"John","age"=>19));
-  var_dump($cursor->getAll());
+    // get documents by example
+    $cursor = $handler->getByExample("users", array("name" => "John", "age" => 19));
+    var_dump($cursor->getAll());
 
-  // get the ids of all documents in the collection
-  $result = $handler->getAllIds("users");
-  var_dump($result);
+    // get the ids of all documents in the collection
+    $result = $handler->getAllIds("users");
+    var_dump($result);
 
-  // create another new document
-  $user = new Document();
-  $user->set("name", "j-lo");
-  $user->level = 1;
-  $user->vists = array(1, 2, 3);
+    // create another new document
+    $user = new Document();
+    $user->set("name", "j-lo");
+    $user->level = 1;
+    $user->vists = array(1, 2, 3);
 
-  $id = $handler->add("users", $user);
-  var_dump("CREATED A NEW DOCUMENT WITH ID: ", $id);
+    $id = $handler->add("users", $user);
+    var_dump("CREATED A NEW DOCUMENT WITH ID: ", $id);
 
-  // get this document from the server
-  $userFromServer = $handler->getById("users", $id);
-  var_dump($userFromServer);
+    // get this document from the server
+    $userFromServer = $handler->getById("users", $id);
+    var_dump($userFromServer);
 
-  // update this document
-  $userFromServer->nonsense = "hihi";
-  unset($userFromServer->name);
-  $result = $handler->update($userFromServer);
-  var_dump($result);
+    // update this document
+    $userFromServer->nonsense = "hihi";
+    unset($userFromServer->name);
+    $result = $handler->update($userFromServer);
+    var_dump($result);
 
-  // get the updated document back
-  $result = $handler->get("users", $id);
-  var_dump($result);
+    // get the updated document back
+    $result = $handler->get("users", $id);
+    var_dump($result);
 
-  // delete the document
-  $result = $handler->deleteById("users", $id);
-  var_dump($result);
-
+    // delete the document
+    $result = $handler->deleteById("users", $id);
+    var_dump($result);
 } catch (ConnectException $e) {
-  print $e . PHP_EOL;
+    print $e . PHP_EOL;
 } catch (ServerException $e) {
-  print $e . PHP_EOL;
+    print $e . PHP_EOL;
 } catch (ClientException $e) {
-  print $e . PHP_EOL;
+    print $e . PHP_EOL;
 }
--- examples/select.php
+++ examples/select.php
@@ -6,34 +6,34 @@ require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'init.php';
 
 /* set up some example statements */
 $statements = array(
-  "for u in users return u" => array(),
-  "for u in users return u" => NULL,
-  "for u in users filter u.id == @id return u" => array("id" => 6),
-  "for u in users filter u.id == @id && u.name != @name return u" => array("id" => 1, "name" => "fox"),
+    "for u in users return u"                                       => array(),
+    "for u in users return u"                                       => null,
+    "for u in users filter u.id == @id return u"                    => array("id" => 6),
+    "for u in users filter u.id == @id && u.name != @name return u" => array("id" => 1, "name" => "fox"),
 );
 
 
 try {
-  $connection = new Connection($connectionOptions);
+    $connection = new Connection($connectionOptions);
 
-  foreach ($statements as $query => $bindVars) {
-    $statement = new Statement($connection, array(
-      "query" => $query,
-      "count" => true,
-      "batchSize" => 1000,
-      "bindVars" => $bindVars,
-      "sanitize" => true,
-    ));
+    foreach ($statements as $query => $bindVars) {
+        $statement = new Statement($connection, array(
+                                                     "query"     => $query,
+                                                     "count"     => true,
+                                                     "batchSize" => 1000,
+                                                     "bindVars"  => $bindVars,
+                                                     "sanitize"  => true,
+                                                ));
 
-    print $statement."\n\n";
+        print $statement . "\n\n";
 
-    $cursor = $statement->execute();
-    var_dump($cursor->getAll());
-  }
+        $cursor = $statement->execute();
+        var_dump($cursor->getAll());
+    }
 } catch (ConnectException $e) {
-  print $e . PHP_EOL;
+    print $e . PHP_EOL;
 } catch (ServerException $e) {
-  print $e . PHP_EOL;
+    print $e . PHP_EOL;
 } catch (ClientException $e) {
-  print $e . PHP_EOL;
+    print $e . PHP_EOL;
 }
