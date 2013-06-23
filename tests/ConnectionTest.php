arangodb/arangodb-php

commit f98463250eae1aa1fd1d510b7aeb412b36438436

changed namespace from triagens to triagens/Avocado, improved documentation

lib/Connection.php
@@ -8,10 +8,10 @@
  * @copyright Copyright 2012, triagens GmbH, Cologne, Germany
  */
 
-namespace triagens;
+namespace triagens\Avocado;
 
 /**
- * AvocadoConnection
+ * Connection
  * 
  * Provides access to the AvocadoDB server
  * As all access is done using HTTP, we do not need to establish a
@@ -21,7 +21,7 @@ namespace triagens;
  *
  * @package AvocadoDbPhpClient
  */
-class AvocadoConnection {
+class Connection {
   /**
    * Connection options
    * @var array 
@@ -31,72 +31,72 @@ class AvocadoConnection {
   /**
    * Set up the connection object, validate the options provided
    *
-   * @throws AvocadoException
-   * @param array $options
+   * @throws Exception
+   * @param array $options - initial connection options
    * @return void
    */
   public function __construct(array $options) {
-    $this->_options = new AvocadoConnectionOptions($options);
+    $this->_options = new ConnectionOptions($options);
   }
   
   /**
    * Issue an HTTP GET request
    *
-   * @throws AvocadoException
-   * @param string $url
-   * @return array
+   * @throws Exception
+   * @param string $url - GET URL
+   * @return HttpResponse
    */
   public function get($url) {
-    $response = $this->executeRequest(AvocadoHttpHelper::METHOD_GET, $url, '');
+    $response = $this->executeRequest(HttpHelper::METHOD_GET, $url, '');
     return $this->parseResponse($response);
   }
   
   /**
    * Issue an HTTP POST request with the data provided
    *
-   * @throws AvocadoException
-   * @param string $url
-   * @param string $data
-   * @return array
+   * @throws Exception
+   * @param string $url - POST URL
+   * @param string $data - body to post
+   * @return HttpResponse
    */
   public function post($url, $data) {
-    $response = $this->executeRequest(AvocadoHttpHelper::METHOD_POST, $url, $data);
+    $response = $this->executeRequest(HttpHelper::METHOD_POST, $url, $data);
     return $this->parseResponse($response);
   }
 
   /**
    * Issue an HTTP PUT request with the data provided
    *
-   * @throws AvocadoException
-   * @param string $url
-   * @param string $data
-   * @return array
+   * @throws Exception
+   * @param string $url - PUT URL
+   * @param string $data - body to post
+   * @return HttpResponse
    */
   public function put($url, $data) {
-    $response = $this->executeRequest(AvocadoHttpHelper::METHOD_PUT, $url, $data);
+    $response = $this->executeRequest(HttpHelper::METHOD_PUT, $url, $data);
     return $this->parseResponse($response);
   }
   
   /**
    * Issue an HTTP DELETE request with the data provided
    *
-   * @throws AvocadoException
-   * @param string $url
-   * @return array
+   * @throws Exception
+   * @param string $url - DELETE URL
+   * @return HttpResponse
    */
   public function delete($url) {
-    $response = $this->executeRequest(AvocadoHttpHelper::METHOD_DELETE, $url, '');
+    $response = $this->executeRequest(HttpHelper::METHOD_DELETE, $url, '');
     return $this->parseResponse($response);
   }
 
   /**
    * Parse the response return the body values as an assoc array
    *
-   * @throws AvocadoException
-   * @param AvocadoHttpResponse $response
-   * @return AvocadoHttpResponse
+   * @throws Exception
+   * @param HttpResponse $response - the response as supplied by the server
+   * @return HttpResponse
    */
-  private function parseResponse(AvocadoHttpResponse $response) {
+  private function parseResponse(HttpResponse $response) {
     $httpCode = $response->getHttpCode();
 
     if ($httpCode < 200 || $httpCode >= 400) {
@@ -109,14 +109,14 @@ class AvocadoConnection {
         $details = json_decode($body, true);
         if (is_array($details)) {
           // yes, we got details
-          $exception = new AvocadoServerException($response->getResult(), $httpCode);
+          $exception = new ServerException($response->getResult(), $httpCode);
           $exception->setDetails($details);
           throw $exception;
         }
       }
 
       // no details found, throw normal exception
-      throw new AvocadoServerException($response->getResult(), $httpCode);
+      throw new ServerException($response->getResult(), $httpCode);
     }
 
     return $response;
@@ -124,18 +124,25 @@ class AvocadoConnection {
   
   /**
    * Execute an HTTP request and return the results
+   * 
+   * This function will throw if no connection to the server can be established or if
+   * there is a problem during data exchange with the server.
+   * The function might temporarily alter the value of the php.ini value 'default_socket_timeout' but
+   * will restore it.
    *
-   * @throws AvocadoException
-   * @param string $method 
-   * @param string $url 
-   * @param string $data 
-   * @return AvocadoHttpResponse
+   * @throws Exception
+   * @param string $method - HTTP request method
+   * @param string $url - HTTP URL
+   * @param string $data - data to post in body
+   * @return HttpResponse
    */
   private function executeRequest($method, $url, $data) {
+    HttpHelper::validateMethod($method);
+
     // create request data
-    $request = AvocadoHttpHelper::buildRequest($this->_options[AvocadoConnectionOptions::OPTION_HOST], $method, $url, $data);
+    $request = HttpHelper::buildRequest($this->_options[ConnectionOptions::OPTION_HOST], $method, $url, $data);
     
-    $traceFunc = $this->_options[AvocadoConnectionOptions::OPTION_TRACE];
+    $traceFunc = $this->_options[ConnectionOptions::OPTION_TRACE];
     if ($traceFunc) {
       // call tracer func
       $traceFunc('send', $request);
@@ -149,26 +156,28 @@ class AvocadoConnection {
       ini_set('default_socket_timeout', $value);
     };
 
-    $scope = new AvocadoScope($getFunc, $setFunc);
-    $setFunc($this->_options[AvocadoConnectionOptions::OPTION_TIMEOUT]);
+    $scope = new Scope($getFunc, $setFunc);
+    $setFunc($this->_options[ConnectionOptions::OPTION_TIMEOUT]);
 
     // open the socket. note: this might throw if the connection cannot be established
-    $connection = AvocadoHttpHelper::createConnection($this->_options);
+    $connection = HttpHelper::createConnection($this->_options);
     if ($connection) {
       // send data and get response back
-      $result = AvocadoHttpHelper::transfer($connection, $request);
+      $result = HttpHelper::transfer($connection, $request);
       // must close the connection
       fclose($connection);
+      $scope->leave();
       
       if ($traceFunc) {
         // call tracer func
         $traceFunc('receive',$result);
       }
 
-      return new AvocadoHttpResponse($result);
+      return new HttpResponse($result);
     } 
 
-    throw new AvocadoClientException('Whoops, this should never happen');
+    $scope->leave();
+    throw new ClientException('Whoops, this should never happen');
   }
 
 }

autoload.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace triagens;
+namespace triagens\Avocado;
 
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'AvocadoAutoloader.php';
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 
-AvocadoAutoloader::init();
+Autoloader::init();
 
-spl_autoload_register(__NAMESPACE__ . '\AvocadoAutoloader::load');
+spl_autoload_register(__NAMESPACE__ . '\Autoloader::load');

docs/classes/db_\triagens\Avocado\Autoloader.html
@@ -1,0 +1,162 @@
+<!DOCTYPE html><html xmlns:date="http://exslt.org/dates-and-times" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<meta charset="utf-8">
+<title>AvocadoDbPhpClient » \triagens\Avocado\Autoloader</title>
+<meta name="author" content="Mike van Riel">
+<meta name="description" content="">
+<link href="../css/template.css" rel="stylesheet" media="all">
+<script src="../js/jquery-1.7.1.min.js" type="text/javascript"></script><script src="../js/jquery-ui-1.8.2.custom.min.js" type="text/javascript"></script><script src="../js/jquery.mousewheel.min.js" type="text/javascript"></script><script src="../js/bootstrap.js" type="text/javascript"></script><script src="../js/template.js" type="text/javascript"></script><script src="../js/prettify/prettify.min.js" type="text/javascript"></script><link rel="shortcut icon" href="../img/favicon.ico">
+<link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../img/apple-touch-icon-114x114.png">
+</head>
+<body>
+<div class="navbar navbar-fixed-top">
+<div class="navbar-inner"><div class="container">
+<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><a class="brand" href="../index.html">AvocadoDbPhpClient</a><div class="nav-collapse"><ul class="nav">
+<li class="dropdown">
+<a href="#api" class="dropdown-toggle" data-toggle="dropdown">
+                                    API Documentation <b class="caret"></b></a><ul class="dropdown-menu">
+<li><a>Namespaces</a></li>
+<li><a href="../namespaces/db_triagens.html"><i class="icon-th"></i> triagens</a></li>
+<li><a>Packages</a></li>
+<li><a href="../packages/db_AvocadoDbPhpClient.html"><i class="icon-folder-open"></i> AvocadoDbPhpClient</a></li>
+</ul>
+</li>
+<li class="dropdown" id="charts-menu">
+<a href="#charts" class="dropdown-toggle" data-toggle="dropdown">
+                                    Charts <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="../graph_class.html"><i class="icon-list-alt"></i> Class hierarchy diagram</a></li></ul>
+</li>
+<li class="dropdown" id="reports-menu">
+<a href="#reports" class="dropdown-toggle" data-toggle="dropdown">
+                                    Reports <b class="caret"></b></a><ul class="dropdown-menu">
+<li><a href="../errors.html"><i class="icon-remove-sign"></i> Errors</a></li>
+<li><a href="../markers.html"><i class="icon-map-marker"></i> Markers (TODO/FIXME)</a></li>
+<li><a href="../deprecated.html"><i class="icon-stop"></i> Deprecated elements</a></li>
+</ul>
+</li>
+</ul></div>
+</div></div>
+<div class="go_to_top"><a href="#___" style="color: inherit">Back to top  <i class="icon-upload icon-white"></i></a></div>
+</div>
+<div id="___" class="container">
+<noscript><div class="alert alert-warning">
+                            Javascript is disabled; several features are only available
+                            if Javascript is enabled.
+                        </div></noscript>
+<div class="row">
+<div class="span4">
+<span class="btn-group visibility" data-toggle="buttons-checkbox"><button class="btn public active" title="Show public elements">Public</button><button class="btn protected" title="Show protected elements">Protected</button><button class="btn private" title="Show private elements">Private</button><button class="btn inherited active" title="Show inherited elements">Inherited</button></span><div class="btn-group view pull-right" data-toggle="buttons-radio">
+<button class="btn details" title="Show descriptions and method names"><i class="icon-list"></i></button><button class="btn simple" title="Show only method names"><i class="icon-align-justify"></i></button>
+</div>
+<ul class="side-nav nav nav-list">
+<li class="nav-header">
+<i class="icon-custom icon-method"></i> Methods</li>
+<li class="method public "><a href="#init" title="init :: Initialise the autoloader"><span class="description">Initialise the autoloader</span><pre>init()</pre></a></li>
+<li class="method public "><a href="#load" title="load :: Handle loading of an unknown class"><span class="description">Handle loading of an unknown class</span><pre>load()</pre></a></li>
+<li class="nav-header private">» Private</li>
+<li class="method private "><a href="#checkEnvironment" title="checkEnvironment :: Check the runtime environment"><span class="description">Check the runtime environment</span><pre>checkEnvironment()</pre></a></li>
+<li class="nav-header">
+<i class="icon-custom icon-property"></i> Properties</li>
+<li class="nav-header private">» Private</li>
+<li class="property private "><a href="#%24libDir" title="$libDir :: Directory with library files"><span class="description">Directory with library files</span><pre>$libDir</pre></a></li>
+<li class="nav-header">
+<i class="icon-custom icon-constant"></i> Constants</li>
+<li class="constant  "><a href="#EXTENSION" title="EXTENSION :: Class file extension"><span class="description">Class file extension</span><pre>EXTENSION</pre></a></li>
+</ul>
+</div>
+<div class="span8">
+<a name="%5Ctriagens%5CAvocado%5CAutoloader" id="\triagens\Avocado\Autoloader"></a><ul class="breadcrumb">
+<li>
+<a href="../index.html"><i class="icon-custom icon-class"></i></a><span class="divider">\</span>
+</li>
+<li><a href="../namespaces/db_triagens.html">triagens</a></li>
+<span class="divider">\</span><li><a href="../namespaces/db_triagens%5CAvocado.html">Avocado</a></li>
+<li class="active">
+<span class="divider">\</span><a href="../classes/db_%5Ctriagens%5CAvocado%5CAutoloader.html">Autoloader</a>
+</li>
+</ul>
+<div href="../classes/db_%5Ctriagens%5CAvocado%5CAutoloader.html" class="element class">
+<p class="short_description">Autoloader</p>
+<div class="details">
+<p class="long_description"><p>Handles automatic loading of missing class files
+The autoloader can be nested with other autoloaders. It will only
+process classes from its own namespace and ignore all others.</p></p>
+<table class="table table-bordered"><tr>
+<th>package</th>
+<td><a href="..//packages/db_AvocadoDbPhpClient.html">AvocadoDbPhpClient</a></td>
+</tr></table>
+<h3>
+<i class="icon-custom icon-method"></i> Methods</h3>
+<a name="init" id="init"></a><div class="element clickable method public init" data-toggle="collapse" data-target=".init .collapse">
+<h2>Initialise the autoloader</h2>
+<pre>init() : void</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"></p>
+<h3>Exceptions</h3>
+<table class="table table-bordered"><tr>
+<th><code><a href="../classes/db_%5Ctriagens%5CAvocado%5CException.html">\triagens\Avocado\Exception</a></code></th>
+<td></td>
+</tr></table>
+</div></div>
+</div>
+<a name="load" id="load"></a><div class="element clickable method public load" data-toggle="collapse" data-target=".load .collapse">
+<h2>Handle loading of an unknown class</h2>
+<pre>load(string $className) : void</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"><p>This will only handle class from its own namespace and ignore all others.
+This allows multiple autoloaders to be used in a nested fashion.</p></p>
+<h3>Parameters</h3>
+<div class="subelement argument">
+<h4>$className</h4>
+<code>string</code><ul>
+<li>name of class to be loaded</li>
+</ul>
+</div>
+</div></div>
+</div>
+<a name="checkEnvironment" id="checkEnvironment"></a><div class="element clickable method private checkEnvironment" data-toggle="collapse" data-target=".checkEnvironment .collapse">
+<h2>Check the runtime environment</h2>
+<pre>checkEnvironment() : void</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"><p>This will check whether the runtime environment is compatible with the
+Avocado PHP client.</p></p>
+<h3>Exceptions</h3>
+<table class="table table-bordered"><tr>
+<th><code><a href="../classes/db_%5Ctriagens%5CAvocado%5CClientException.html">\triagens\Avocado\ClientException</a></code></th>
+<td></td>
+</tr></table>
+</div></div>
+</div>
+<h3>
+<i class="icon-custom icon-property"></i> Properties</h3>
+<a name="%24libDir" id="$libDir"> </a><div class="element clickable property private $libDir" data-toggle="collapse" data-target=".$libDir .collapse">
+<h2>Directory with library files</h2>
+<pre>$libDir : string</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8"><p class="long_description"></p></div></div>
+</div>
+<h3>
+<i class="icon-custom icon-constant"></i> Constants</h3>
+<a name="EXTENSION" id="EXTENSION"> </a><div class="element clickable constant  EXTENSION" data-toggle="collapse" data-target=".EXTENSION .collapse">
+<h2>Class file extension</h2>
+<pre>EXTENSION </pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8"><p class="long_description"></p></div></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="row"><footer class="span12">
+            Template is built using <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap 2</a> and icons provided by <a href="http://glyphicons.com/">Glyphicons</a>.<br>
+            Documentation is powered by <a href="http://www.phpdoc.org/">phpDocumentor 2.0.0a1</a> and<br>
+            generated on 2012-03-21T19:43:05+01:00.<br></footer></div>
+</div>
+</body>
+</html>

docs/classes/db_\triagens\Avocado\BindVars.html
@@ -1,0 +1,172 @@
+<!DOCTYPE html><html xmlns:date="http://exslt.org/dates-and-times" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<meta charset="utf-8">
+<title>AvocadoDbPhpClient » \triagens\Avocado\BindVars</title>
+<meta name="author" content="Mike van Riel">
+<meta name="description" content="">
+<link href="../css/template.css" rel="stylesheet" media="all">
+<script src="../js/jquery-1.7.1.min.js" type="text/javascript"></script><script src="../js/jquery-ui-1.8.2.custom.min.js" type="text/javascript"></script><script src="../js/jquery.mousewheel.min.js" type="text/javascript"></script><script src="../js/bootstrap.js" type="text/javascript"></script><script src="../js/template.js" type="text/javascript"></script><script src="../js/prettify/prettify.min.js" type="text/javascript"></script><link rel="shortcut icon" href="../img/favicon.ico">
+<link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../img/apple-touch-icon-114x114.png">
+</head>
+<body>
+<div class="navbar navbar-fixed-top">
+<div class="navbar-inner"><div class="container">
+<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><a class="brand" href="../index.html">AvocadoDbPhpClient</a><div class="nav-collapse"><ul class="nav">
+<li class="dropdown">
+<a href="#api" class="dropdown-toggle" data-toggle="dropdown">
+                                    API Documentation <b class="caret"></b></a><ul class="dropdown-menu">
+<li><a>Namespaces</a></li>
+<li><a href="../namespaces/db_triagens.html"><i class="icon-th"></i> triagens</a></li>
+<li><a>Packages</a></li>
+<li><a href="../packages/db_AvocadoDbPhpClient.html"><i class="icon-folder-open"></i> AvocadoDbPhpClient</a></li>
+</ul>
+</li>
+<li class="dropdown" id="charts-menu">
+<a href="#charts" class="dropdown-toggle" data-toggle="dropdown">
+                                    Charts <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="../graph_class.html"><i class="icon-list-alt"></i> Class hierarchy diagram</a></li></ul>
+</li>
+<li class="dropdown" id="reports-menu">
+<a href="#reports" class="dropdown-toggle" data-toggle="dropdown">
+                                    Reports <b class="caret"></b></a><ul class="dropdown-menu">
+<li><a href="../errors.html"><i class="icon-remove-sign"></i> Errors</a></li>
+<li><a href="../markers.html"><i class="icon-map-marker"></i> Markers (TODO/FIXME)</a></li>
+<li><a href="../deprecated.html"><i class="icon-stop"></i> Deprecated elements</a></li>
+</ul>
+</li>
+</ul></div>
+</div></div>
+<div class="go_to_top"><a href="#___" style="color: inherit">Back to top  <i class="icon-upload icon-white"></i></a></div>
+</div>
+<div id="___" class="container">
+<noscript><div class="alert alert-warning">
+                            Javascript is disabled; several features are only available
+                            if Javascript is enabled.
+                        </div></noscript>
+<div class="row">
+<div class="span4">
+<span class="btn-group visibility" data-toggle="buttons-checkbox"><button class="btn public active" title="Show public elements">Public</button><button class="btn protected" title="Show protected elements">Protected</button><button class="btn private" title="Show private elements">Private</button><button class="btn inherited active" title="Show inherited elements">Inherited</button></span><div class="btn-group view pull-right" data-toggle="buttons-radio">
+<button class="btn details" title="Show descriptions and method names"><i class="icon-list"></i></button><button class="btn simple" title="Show only method names"><i class="icon-align-justify"></i></button>
+</div>
+<ul class="side-nav nav nav-list">
+<li class="nav-header">
+<i class="icon-custom icon-method"></i> Methods</li>
+<li class="method public "><a href="#get" title="get :: Get the value of a bind variable with a specific name"><span class="description">Get the value of a bind variable with a specific name</span><pre>get()</pre></a></li>
+<li class="method public "><a href="#getAll" title="getAll :: Get all registered bind variables"><span class="description">Get all registered bind variables</span><pre>getAll()</pre></a></li>
+<li class="method public "><a href="#getCount" title="getCount :: Get the number of bind variables registered"><span class="description">Get the number of bind variables registered</span><pre>getCount()</pre></a></li>
+<li class="method public "><a href="#set" title="set :: Set the value of a single bind variable or set all bind variables at once
+This will also validate the bind values."><span class="description">Set the value of a single bind variable or set all bind variables at once
+This will also validate the bind values.</span><pre>set()</pre></a></li>
+<li class="nav-header">
+<i class="icon-custom icon-property"></i> Properties</li>
+<li class="nav-header private">» Private</li>
+<li class="property private "><a href="#%24_values" title="$_values :: Current bind values"><span class="description">Current bind values</span><pre>$_values</pre></a></li>
+</ul>
+</div>
+<div class="span8">
+<a name="%5Ctriagens%5CAvocado%5CBindVars" id="\triagens\Avocado\BindVars"></a><ul class="breadcrumb">
+<li>
+<a href="../index.html"><i class="icon-custom icon-class"></i></a><span class="divider">\</span>
+</li>
+<li><a href="../namespaces/db_triagens.html">triagens</a></li>
+<span class="divider">\</span><li><a href="../namespaces/db_triagens%5CAvocado.html">Avocado</a></li>
+<li class="active">
+<span class="divider">\</span><a href="../classes/db_%5Ctriagens%5CAvocado%5CBindVars.html">BindVars</a>
+</li>
+</ul>
+<div href="../classes/db_%5Ctriagens%5CAvocado%5CBindVars.html" class="element class">
+<p class="short_description">BindVars</p>
+<div class="details">
+<p class="long_description"><p>A simple container for bind variables
+This container also handles validation of the bind values.</p></p>
+<table class="table table-bordered"><tr>
+<th>package</th>
+<td><a href="..//packages/db_AvocadoDbPhpClient.html">AvocadoDbPhpClient</a></td>
+</tr></table>
+<h3>
+<i class="icon-custom icon-method"></i> Methods</h3>
+<a name="get" id="get"></a><div class="element clickable method public get" data-toggle="collapse" data-target=".get .collapse">
+<h2>Get the value of a bind variable with a specific name</h2>
+<pre>get(string $name) : mixed</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"></p>
+<h3>Parameters</h3>
+<div class="subelement argument">
+<h4>$name</h4>
+<code>string</code><p>name - name of bind variable</p>
+</div>
+<h3>Returns</h3>
+<div class="subelement response">
+<code>mixed</code>- value of bind variable</div>
+</div></div>
+</div>
+<a name="getAll" id="getAll"></a><div class="element clickable method public getAll" data-toggle="collapse" data-target=".getAll .collapse">
+<h2>Get all registered bind variables</h2>
+<pre>getAll() : array</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"></p>
+<h3>Returns</h3>
+<div class="subelement response">
+<code>array</code>- array of all registered bind variables</div>
+</div></div>
+</div>
+<a name="getCount" id="getCount"></a><div class="element clickable method public getCount" data-toggle="collapse" data-target=".getCount .collapse">
+<h2>Get the number of bind variables registered</h2>
+<pre>getCount() : int</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"></p>
+<h3>Returns</h3>
+<div class="subelement response">
+<code>int</code>- number of bind variables registered</div>
+</div></div>
+</div>
+<a name="set" id="set"></a><div class="element clickable method public set" data-toggle="collapse" data-target=".set .collapse">
+<h2>Set the value of a single bind variable or set all bind variables at once
+This will also validate the bind values.</h2>
+<pre>set(mixed $name, string $value) : void</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8">
+<p class="long_description"><p>Allowed value types for bind parameters are string, int,
+double, bool and array. Arrays must not contain any other
+than these types.</p></p>
+<h3>Parameters</h3>
+<div class="subelement argument">
+<h4>$name</h4>
+<code>mixed</code><p>name - name of bind variable OR an array with all bind variables</p>
+</div>
+<div class="subelement argument">
+<h4>$value</h4>
+<code>string</code><p>value - value for bind variable</p>
+</div>
+<h3>Exceptions</h3>
+<table class="table table-bordered"><tr>
+<th><code><a href="../classes/db_%5Ctriagens%5CAvocado%5CClientException.html">\triagens\Avocado\ClientException</a></code></th>
+<td></td>
+</tr></table>
+</div></div>
+</div>
+<h3>
+<i class="icon-custom icon-property"></i> Properties</h3>
+<a name="%24_values" id="$_values"> </a><div class="element clickable property private $_values" data-toggle="collapse" data-target=".$_values .collapse">
+<h2>Current bind values</h2>
+<pre>$_values : array</pre>
+<div class="labels"></div>
+<div class="row collapse"><div class="span8"><p class="long_description"></p></div></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="row"><footer class="span12">
+            Template is built using <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap 2</a> and icons provided by <a href="http://glyphicons.com/">Glyphicons</a>.<br>
+            Documentation is powered by <a href="http://www.phpdoc.org/">phpDocumentor 2.0.0a1</a> and<br>
+            generated on 2012-03-21T19:43:05+01:00.<br></footer></div>
+</div>
+</body>
+</html>

commit 3d04d4df324fd9dc7d540e8957c81077472f8898

Fixed enhanced tracer headers that got broken in f98463250eae1aa1fd1d510b7aeb412b36438436.

lib/triagens/ArangoDb/Connection.php
@@ -332,7 +332,7 @@ class Connection
             // call tracer func
             if ($this->_options[ConnectionOptions::OPTION_ENHANCED_TRACE]) {
                 list($header) = HttpHelper::parseHttpMessage($request);
-                $traceFunc(new TraceRequest(HttpHelper::parseHeaders($header), $method, $url, $data));
+                $traceFunc(new TraceRequest(HttpHelper::parseHeaders($header)[2], $method, $url, $data));
             } else {
                 $traceFunc('send', $request);
             }

tests/ConnectionTest.php
@@ -137,6 +137,12 @@ class ConnectionTest extends
 
                 $self->assertInternalType('string', $data->getRequestUrl(), 'Request url must be a string!');
                 $self->assertEquals('request', $data->getType());
+                
+                foreach ($data->getHeaders() as $header => $value) {
+                	$this->assertInternalType('string', $value, "The header value should be a string");
+                	$this->assertInternalType('string', $header, "The header should be a string");
+                }
+                
             } else {
                 $self->assertInternalType('integer', $data->getHttpCode(), 'Http code must be an integer!');
                 $self->assertInternalType(