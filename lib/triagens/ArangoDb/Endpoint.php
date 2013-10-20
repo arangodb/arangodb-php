dedc529048fe18bbec7d78fbf5f3b7cd2687a889
Frank Mayer <frank@frankmayer.net>
2013-10-20 23:42:57 +0300

    Implemented Endpoint API support by extending the existent endpoint class. Closes #154

--- lib/triagens/ArangoDb/Endpoint.php
+++ lib/triagens/ArangoDb/Endpoint.php
@@ -68,6 +68,17 @@ class Endpoint
     const REGEXP_UNIX = '/^unix:\/\/(.+)$/';
 
     /**
+     * Endpoint index
+     */
+    const ENTRY_ENDPOINT = 'endpoint';
+
+    /**
+     * Databases index
+     */
+    const ENTRY_DATABASES = 'databases';
+
+
+    /**
      * Create a new endpoint
      *
      * @param string $value - endpoint specification
@@ -157,5 +168,92 @@ class Endpoint
         }
 
         return true;
+    }
+
+
+    /**
+     * creates an endpoint
+     *
+     * This creates a new endpoint<br>
+     * This is an alias function to Endpoint::modify, as ArangoDB's API has one call to support both new and modify
+     *
+     * @param Connection $connection - the connection to be used
+     * @param string     $endpoint   - the endpoint specification, e.g. tcp://127.0.0.1:8530
+     * @param array      $databases  - a list of database names the endpoint is responsible for.
+     *                               *
+     *
+     * @link http://www.arangodb.org/manuals/1.4/HttpEndpoint.html
+     * @return array $responseArray - The response array.
+     */
+    public static function create(Connection $connection, $endpoint, array $databases)
+    {
+        return self::modify($connection, $endpoint, $databases);
+    }
+
+
+    /**
+     * modifies an endpoint
+     *
+     * This will modify an existing or create a new endpoint.
+     *
+     * @param Connection $connection - the connection to be used
+     * @param string     $endpoint   - the endpoint specification, e.g. tcp://127.0.0.1:8530
+     * @param array      $databases  - a list of database names the endpoint is responsible for.
+     *
+     * @link http://www.arangodb.org/manuals/1.4/HttpEndpoint.html
+     * @return array $responseArray - The response array.
+     */
+    public static function modify(Connection $connection, $endpoint, array $databases)
+    {
+        $payload = array(self::ENTRY_ENDPOINT => $endpoint, self::ENTRY_DATABASES => $databases);
+
+        $response = $connection->post(Urls::URL_ENDPOINT, $connection->json_encode_wrapper($payload));
+
+        $responseArray = $response->getJson();
+
+        return $responseArray;
+    }
+
+
+    /**
+     * Deletes an endpoint
+     *
+     * This will delete an existing endpoint.
+     *
+     * @param Connection $connection - the connection to be used
+     * @param string     $endpoint   - the endpoint specification, e.g. tcp://127.0.0.1:8530
+     *
+     * @link                         http://www.arangodb.org/manuals/1.4/HttpEndpoint.html
+     * @return array $responseArray - The response array.
+     */
+    public static function delete(Connection $connection, $endpoint)
+    {
+        $url = UrlHelper::buildUrl(Urls::URL_ENDPOINT, array($endpoint));
+
+        $response = $connection->delete($url);
+
+        $responseArray = $response->getJson();
+
+        return $responseArray;
+    }
+
+
+    /**
+     * List endpoints
+     *
+     * This will list the endpoints that are configured on the server
+     *
+     * @param Connection $connection - the connection to be used
+     *
+     * @link                         http://www.arangodb.org/manuals/1.4/HttpEndpoint.html
+     * @return array $responseArray - The response array.
+     */
+    public static function listEndpoints(Connection $connection)
+    {
+        $response = $connection->get(Urls::URL_ENDPOINT);
+
+        $responseArray = $response->getJson();
+
+        return $responseArray;
     }
 }
