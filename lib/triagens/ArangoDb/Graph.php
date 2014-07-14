c4ff9d69e73768e36ae9239f1dbf3d2c1a925fc7
scottashton <f.bartels@triagens.de>
2014-07-14 10:16:28 +0200

    make api compatible with 5.3

--- lib/triagens/ArangoDb/Graph.php
+++ lib/triagens/ArangoDb/Graph.php
@@ -120,7 +120,8 @@ class Graph extends
     		return null;
     	}
     	$this->addEdgeDefinition($edgeDefinition);
-    	return $edgeDefinition->getFromCollections()[0];
+    	$fc = $edgeDefinition->getFromCollections();
+    	return $fc[0];
     }
 
     /**
@@ -260,21 +261,22 @@ class Graph extends
      * @return EdgeDefinition
      */
     private function getSingleUndirectedRelation() {
-    	if (count($this->getEdgeDefinitions()) > 1 || 
+    	$ed = $this->getEdgeDefinitions();
+    	if (count($ed) > 1 || 
         	(
-        		count($this->getEdgeDefinitions()) === 1 && (
-            			count($this->getEdgeDefinitions()[0]->getFromCollections()) > 1 ||
-            			count($this->getEdgeDefinitions()[0]->getToCollections()) > 1 ||
-        				$this->getEdgeDefinitions()[0]->getFromCollections()[0] !== 
-        				$this->getEdgeDefinitions()[0]->getToCollections()[0]
+        		count($ed) === 1 && (
+            			count($ed[0]->getFromCollections()) > 1 ||
+            			count($ed[0]->getToCollections()) > 1 ||
+        				$ed[0]->getFromCollections()[0] !== 
+        				$ed[0]->getToCollections()[0]
         			
     			)
         	)
         ) {
     		throw new ClientException('This operation only supports graphs with one undirected single collection relation'); 	
     	}
-  		if (count($this->getEdgeDefinitions()) === 1) {
-  			$eD =  $this->getEdgeDefinitions()[0];
+  		if (count($ed) === 1) {
+  			$eD =  $ed[0];
   			$this->_edgeDefinitions = array();
   		} else {
   			$eD = new EdgeDefinition();
