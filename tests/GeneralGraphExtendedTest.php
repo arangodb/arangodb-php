47330d2740eb0077c813ea1eaa0db7ca1d90282b
scottashton <f.bartels@triagens.de>
2014-07-14 09:35:14 +0200

    make api compatible with 5.3

--- tests/GeneralGraphExtendedTest.php
+++ tests/GeneralGraphExtendedTest.php
@@ -30,10 +30,12 @@ class GeneralGraphExtendedTest extends
     	$this->v5 = "v5" . $v;
     	$this->e1 = "e1" . $v;
     	$this->e2 = "e2" . $v;
+    	$param1 = [$this->v1, $this->v2];
+    	$param2 = [$this->v3, $this->v4];
         $ed1 = EdgeDefinition::createDirectedRelation(
         		$this->e1, 
-        		[$this->v1, $this->v2], 
-        		[$this->v3, $this->v4]
+        		$param1, 
+        		$param2
 		);
     	$ed2 = EdgeDefinition::createUndirectedRelation(
     			$this->e2, $this->v5
