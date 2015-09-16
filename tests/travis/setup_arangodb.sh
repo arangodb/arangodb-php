0ceeab1fafcfd545da3182b22f53aaf7914977f4
Jan Steemann <jan@arangodb.com>
2015-09-16 14:54:04 +0200

    try modified setup script

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -52,10 +52,19 @@ if [ "x$process" == "x" ]; then
 fi
 
 echo "Waiting until ArangoDB is ready on port 8529"
-while [[ -z `curl -s 'http://127.0.0.1:8529/_api/version' ` ]] ; do
+
+n=0
+timeout=60
+while [[ (-z `curl -H 'Authorization: Basic cm9vdDo=' -s 'http://127.0.0.1:8529/_api/version' `) && (n -lt timeout) ]] ; do
   echo -n "."
-  sleep 2s
+  sleep 1s
+  n=$[$n+1]
 done
+if [[ n -eq timeout ]];
+then
+    echo "Could not start ArangoDB. Timeout reached."
+    exit 1
+fi
 
 echo "ArangoDB is up"
 
