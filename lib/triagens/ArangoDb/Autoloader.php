d5fb764d6cbcd67188bb961b16b33b1d1cb77c52
frank <frank@frankmayer.net>
2016-10-23 16:17:08 +0300

    Raised minimum PHP version to 5.5 in autoloader

--- lib/triagens/ArangoDb/Autoloader.php
+++ lib/triagens/ArangoDb/Autoloader.php
@@ -89,8 +89,8 @@ class Autoloader
     {
         list($major, $minor) = explode('.', phpversion());
 
-        if ((int) $major < 5 || ((int) $major === 5 && (int) $minor < 3)) {
-            throw new ClientException('Incompatible PHP environment. Expecting PHP 5.3 or higher');
+        if ((int) $major < 5 || ((int) $major === 5 && (int) $minor < 5)) {
+            throw new ClientException('Incompatible PHP environment. Expecting PHP 5.5 or higher');
         }
     }
 }
