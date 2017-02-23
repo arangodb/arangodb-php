7dfee54a96903a20ca6f3e966a5935294f01e508
jsteemann <jan@arangodb.com>
2017-02-23 14:24:08 +0100

    fix assertion that is wrong when using 'Connection: Keep-Alive'

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -67,7 +67,7 @@ class ConnectionTest extends
         static::assertEquals(12, $value);
 
         $value = $connection->getOption(ConnectionOptions::OPTION_CONNECTION);
-        static::assertEquals('Close', $value);
+        static::assertTrue($value === 'Close' || $value === 'Keep-Alive');
 
         $value = $connection->getOption(ConnectionOptions::OPTION_RECONNECT);
         static::assertFalse($value);
