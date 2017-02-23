c477734f470f7f4f61da3d89a5de65938bab43d5
jsteemann <jan@arangodb.com>
2017-02-23 15:46:08 +0100

    try to fix tests with newer versions of PHPUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,10 @@ namespace triagens\ArangoDb;
 
 require __DIR__ . '/..' . '/autoload.php';
 
+if (class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias(\PHPUnit\Framework\TestCase::class, 'PHPUnit_Framework_TestCase');
+}
+
 /* set up a trace function that will be called for each communication with the server */
 
 function isCluster(Connection $connection)
