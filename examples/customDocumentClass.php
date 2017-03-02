40bac9165103052dc40b96f3d27977b79be2e2b4
frank <frank@frankmayer.net>
2017-03-02 11:12:50 +0200

    Fix access modifier + set ArangoDB-PHP version

--- examples/customDocumentClass.php
+++ examples/customDocumentClass.php
@@ -75,9 +75,9 @@ abstract class AbstractEntity extends Document implements \JsonSerializable
      * @link  http://php.net/manual/en/jsonserializable.jsonserialize.php
      * @return mixed data which can be serialized by <b>json_encode</b>,
      *        which is a value of any type other than a resource.
-     * @since 5.4.0
+     * @since 3.2
      */
-    function jsonSerialize()
+    public function jsonSerialize()
     {
         return $this->getAll();
     }
