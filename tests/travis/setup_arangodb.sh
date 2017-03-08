d3635b91925f6526b492f04b2951873a69abd4bb
jsteemann <jan@arangodb.com>
2017-03-08 17:03:44 +0100

    do not use nightly, but devel build

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "cd $DIR"
 cd "$DIR"
 
-VERSION="devel-nightly"
+VERSION="devel"
 NAME="ArangoDB-$VERSION"
 
 if [ ! -d "$DIR/$NAME" ]; then
