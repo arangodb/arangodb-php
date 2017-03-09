67ef9d23f71ac1848f6adfa8f5953cc932962d68
jsteemann <jan@arangodb.com>
2017-03-09 15:14:59 +0100

    next try

--- tests/bootstrap-connection-keep-alive.php
+++ tests/bootstrap-connection-keep-alive.php
@@ -11,9 +11,9 @@ namespace ArangoDBClient;
 
 require __DIR__ . '/../autoload.php';
 
-//if (class_exists('\PHPUnit\Framework\TestCase')) {
-//    class_alias(\PHPUnit\Framework\TestCase::class, 'PHPUnit_Framework_TestCase');
-//}
+if (class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias(\PHPUnit\Framework\TestCase::class, 'PHPUnit_Framework_TestCase');
+}
 
 /* set up a trace function that will be called for each communication with the server */
 
