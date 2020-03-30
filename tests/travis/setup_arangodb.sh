fbc337f8eb537983d4f0dc3dc5dc8ed0c9aa65e9
jsteemann <jan@arangodb.com>
2020-03-30 21:28:04 +0200

    use different Docker image for testing

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -35,8 +35,8 @@ echo "./phpunit --version"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-docker pull arangodb/arangodb-preview:3.7.0-preview.1
-docker run -d -e ARANGO_ROOT_PASSWORD="test" -p 8529:8529 arangodb/arangodb-preview:3.7.0-preview.1
+docker pull arangodb/arangodb-test:3.7-nightly
+docker run -d -e ARANGO_ROOT_PASSWORD="test" -p 8529:8529 arangodb/arangodb-test:3.7-nightly
 
 sleep 2
 
