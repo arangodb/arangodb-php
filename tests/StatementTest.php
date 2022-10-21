d092247b24fb2b2290e098afa17145589a739d3d
Tom Regner <tomte@tomsdiner.org>
2022-10-21 16:13:47 +0200

    Revert "Change StatementTest to accept the servers behaviour."
    This reverts commit a07135f76a8613ebb4ef45b5f9dd4be0f31f0401.

--- tests/StatementTest.php
+++ tests/StatementTest.php
@@ -275,6 +275,11 @@ class StatementTest extends
                 'scannedFull'         => 0,
                 'scannedIndex'        => 0,
                 'filtered'            => 0,
+                'cursorsCreated'      => 0,
+                'cursorsRearmed'      => 0,
+                'cacheHits'           => 0,
+                'cacheMisses'         => 0,
+                'intermediateCommits' => 0,
             ], filtered($extra['stats'])
         );
 
@@ -313,6 +318,11 @@ class StatementTest extends
                 'scannedFull'         => 0,
                 'scannedIndex'        => 3,
                 'filtered'            => 0,
+                'cursorsCreated'      => 1,
+                'cursorsRearmed'      => 0,
+                'cacheHits'           => 0,
+                'cacheMisses'         => 0,
+                'intermediateCommits' => 0,
             ], filtered($extra['stats'])
         );
 
@@ -351,6 +361,11 @@ class StatementTest extends
                 'scannedFull'         => 1000,
                 'scannedIndex'        => 0,
                 'filtered'            => 500,
+                'cursorsCreated'      => 0,
+                'cursorsRearmed'      => 0,
+                'cacheHits'           => 0,
+                'cacheMisses'         => 0,
+                'intermediateCommits' => 0,
             ], filtered($extra['stats'])
         );
 
